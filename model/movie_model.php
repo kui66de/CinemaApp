8e878db6ed612cfee8460f5c5acb37860ab72694
Zy17 <33502377+zyang19941115@users.noreply.github.com>
2019-03-28 23:06:59 +0800

    function of movie model

---
+++ model/movie_model.php
@@ -0,0 +1,68 @@
+<?php
+function get_movies() {
+    global $db;
+    $query = 'SELECT * FROM movies WHERE is_deleted = 0
+              ORDER BY id';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $movies = $statement->fetchAll();
+    $statement->closeCursor();
+    return $movies;
+}
+
+function get_movies_by_genre($genre) {
+    global $db;
+    $query = 'SELECT * FROM movies
+              WHERE movies.genre = :genre AND is_deleted = 0
+              ORDER BY id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":genre", $genre);
+    $statement->execute();
+    $movies = $statement->fetchAll();
+    $statement->closeCursor();
+    return $movies;
+}
+function get_movie($id) {
+    global $db;
+    $query = 'SELECT * FROM movies
+              WHERE id = :id AND is_deleted = 0';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":id", $id);
+    $statement->execute();
+    $movie = $statement->fetch();
+    $statement->closeCursor();
+    return $movie;
+}
+
+/**
+ * @param $game_id
+ * use soft delete
+ */
+function delete_movie($id) {
+    global $db;
+    $query = "UPDATE movies 
+              set is_deleted = 1 
+              WHERE id = :id";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function add_movie($id, $genre, $title, $director,$year, $price) {
+    global $db;
+    $query = 'INSERT INTO movies
+                 (id, genre, title, director, year, price)
+              VALUES
+                 (:id, :genre, :title, :director, :year, :price)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':id', $id);
+    $statement->bindValue(':genre', $genre);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':director', $director);
+    $statement->bindValue(':year', $year);
+    $statement->bindValue(':price', $price);
+    $statement->execute();
+    $statement->closeCursor();
+}
+php?>
