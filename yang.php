ee5732a0b7194ac32a5dba8932e621258fe76a61
dpfei <duanpengfe@163.com>
2019-04-16 15:56:34 +0800

    修改路由

--- yang.php
+++ yang.php
@@ -2,8 +2,11 @@
 
 require('controller/movie.php');
 require('controller/user.php');
+require ('controller/admin.php');
 require('model/database.php');
 require('model/movie_model.php');
+require('model/user_model.php');
+require('model/admin_model.php');
 require('functions.php');
 
 
@@ -19,6 +22,37 @@ switch ($action) {
     case 'list_movies':
         echo getMovies();
         break;
+    case 'get_movie':
+        echo get_single_movie();
+        break;
+    case 'add':
+        echo add_new_movie();
+        break;
+    case 'update':
+        echo modify_movie();
+        break;
+    case 'delete':
+        echo del_movie();
+        break;
+    case 'buy':
+        echo buy_movie();
+        break;
     case 'user_register':
         echo user_register();
+        break;
+    case 'user_login':
+        echo user_login();
+        break;
+    case 'user_logout':
+        echo user_logout();
+        break;
+    case 'admin_login':
+        echo admin_login();
+        break;
+    case 'admin_logout':
+        echo admin_logout();
+        break;
+    default:
+        echo getMovies();
+        break;
 }
