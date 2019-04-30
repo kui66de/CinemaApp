ae2fbd0320690fed4c24e6a136f52578d025d663
Zy100817 <yz941115@gmail.com>
2019-04-30 22:19:30 +0800

    small edit navbar

--- views/nav.php
+++ views/nav.php
@@ -22,6 +22,7 @@
                     }
                     else{
                         ?><li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>
                         <?php
                     }
                     ?>
